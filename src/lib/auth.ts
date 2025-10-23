3949b0d32326a56428fe691253512fb217ebb46c
fikiryilkal tages <fikeryilkaltages@gmail.com>
2025-10-23 10:03:32 +0300

    fix: added www to mykalat

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,7 +18,7 @@ export const auth = betterAuth({
     "http://localhost:3000",
     "http://172.22.84.96:3000",
     "http://172.22.85.157:3000",
-    "https://mykalat.com",
+    "https://www.mykalat.com/",
     "https://promolink-git-feature-chat-fikiryilkal-tages-projects.vercel.app",
     "https://promolink-git-feature-business-fikiryilkal-tages-projects.vercel.app",
     "https://promolink-kgdf93dee-fikiryilkal-tages-projects.vercel.app",
